fc34fd28b39833cab41403c0469adce36ca54ef7
inversionesrc1886ca3-ai <inversionesrc1886ca3@gmail.com>
2026-03-26 02:49:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 import threading
 from datetime import datetime as dt # Corregido para evitar conflicto con el módulo
 
-app = Flask(_name_)
+app = Flask(__name__)
 CORS(app)
 
 # --- CONFIGURACIÓN DINÁMICA POR ENTORNO ---
